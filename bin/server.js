b08bd11451ab34b067d02da9c014b59e1f948803
Rodrigo Folco <rodrigo.folco@linkapi.com.br>
2019-05-07 13:14:42 -0300

    mongodb setup add

--- bin/server.js
+++ bin/server.js
@@ -2,9 +2,24 @@
 const app = require('../src/app');
 const http = require('http');
 const debug = require('debug')('api-hunter:server');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const port = normalizePort(process.env.PORT || '3000');
+
+const port = normalizePort(process.env.PORT);
 app.set('port', port)
+
+const uri = process.env.MONGO_URI;
+const options = { reconnectTries: Number.MAX_VALUE, reconnectInterval: 500, poolSize: 5, useNewUrlParser: true };
+mongoose.connect(uri, options);
+
+mongoose.connection.on('error', () => {
+    console.log('Error trying to connect to the database.')
+})
+
+mongoose.connection.on('connected', () => {
+    console.log('Database connection successfully established.');
+})
 
 const server = http.createServer(app);
 server.listen(port);
@@ -48,7 +63,7 @@ function onError(error) {
     }
 }
 
-function onListening()  {
+function onListening() {
     const addr = server.address();
     const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
     debug('Listening on ' + bind);
