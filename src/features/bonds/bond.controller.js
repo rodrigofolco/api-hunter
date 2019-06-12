f8f8555d0deff125167b5f4c57f5504236153515
Rodrigo Folco <rodrigo.folco@linkapi.com.br>
2019-06-11 23:22:58 -0300

    user bonds load add

--- src/features/bonds/bond.controller.js
+++ src/features/bonds/bond.controller.js
@@ -8,19 +8,12 @@ const { ObjectId } = require('mongodb');
 router.get('/', async (req, res) => {
     try {
         const { query } = req;
-        console.log('query :', query);
         let userBonds;
         let { body } = await request('GET', 'http://jurus.com.br/api/v1/bonds');
-        // console.log('body :', body);
-
         if (query.owner) {
-            console.log('oie');
-            userBonds = (await bonds.find({ owner: ObjectId(query.owner) })).map(bond => bond._id.toString());
-            console.log('userBonds :', userBonds);
-            console.log('body.bonds :', body.bonds.length);
-            body.bonds = body.bonds.filter(bond => userBonds.includes(bond._id.toString));
+            userBonds = (await bonds.find({ owner: ObjectId(query.owner) })).map(bond => bond.bond.toString());
+            body.bonds = body.bonds.filter(bond => userBonds.includes(bond._id));
         }
-        console.log('body.bonds :', body.bonds);
         return res.send({ data: body.bonds });
     } catch (error) {
         return res.status(500).send({ error: 'Error trying to get bonds.' });
